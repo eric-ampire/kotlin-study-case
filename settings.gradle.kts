67a4da6ce413c196784cf8325e712286aead10c3
Eric Ampire <ericampire.top@gmail.com>
2021-08-28 10:16:01 +0200

    Adding module for tdd and bdd

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 
 rootProject.name = "kotlin-study-case"
 include("design-pattern")
+include("tdd")
+include("bdd")
---
+++ tdd/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.ericampire.tdd"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
