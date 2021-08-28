67b134f48a7fdbf26d94633d203bdc4569726fa5
Eric Ampire <ericampire.top@gmail.com>
2021-08-28 10:11:53 +0200

    Adding Gradle file for the child module

---
+++ design-pattern/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.ericampire"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
